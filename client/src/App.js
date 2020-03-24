28f97822cc4c548c9e6005607fe8650b66c3fd36
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-24 14:29:47 -0600

    user logout works, require signin to access full website

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,23 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
+import ProtectedRoute from './components/ProtectedRoute';
+import FetchUser from './components/FetchUser';
 
 function App() {
   return (
     <Fragment>
       <Navbar />
+      <FetchUser> 
       <Container> 
         <Switch>
-          <Route exact path='/' component={Home} />
+          <ProtectedRoute Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/register' component={Register} />
           <Route component={NoMatch} />
         </Switch>
       </Container>
+      </FetchUser>
     </Fragment>
   );
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_200000) do
+ActiveRecord::Schema.define(version: 2020_03_24_195408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "date"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.integer "likes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -45,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_03_23_200000) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
