82c2135a61ef2bf39ec22dab85ec182a8303a4b9
edd23 <yahired3@gmail.com>
2020-03-24 19:25:43 -0600

    finished post_controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,10 +1,18 @@
 class Api::PostsController < ApplicationController
-
   def index
     user = User.find(params[:user_id])
     render json: user.posts
   end
-
-  def update
+  def create
+    user = User.find(params[:user_id])
+    post = user.post.new(post_params)
+    render json: user.post.create(post_params)
+  end
+  def destroy
+    post.find(params[:id]).destroy
+  end
+  private
+  def post_params
+    params.require(:post).permit(:name)
   end
 end
